78379dfe718310e7e99bf3ee69d7296929d043f3
James Lunt <50912137+james-lunt@users.noreply.github.com>
2021-08-10 13:37:08 +0100

    Google sheets API working

---
+++ SheetsDatabase.py
@@ -0,0 +1,42 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+# scope of the application
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    "creds.json", scope)
+
+client = gspread.authorize(credentials)
+
+# Open the spreadhseet
+sheet = client.open("Zalando Data").worksheet("zalando_data")
+
+# Get a list of all records
+data = sheet.get_all_records()
+#pprint(data)
+values_list = sheet.col_values(1)
+
+pprint(len(values_list))
+
+
+# Insert the list as a row at index 8
+#insertRow = ["GA111A28J-A11","Gabor","Trainers — weiß/ice","https://img01.ztat.net/article/spp-media-p1/bc1c433d1649346e9eb02d316961bfc9/dd8d3ad466d64631b956405046eb9ca1.jpg","£89.99","https://www.zalando.co.uk/gabor-trainers-weissice-ga111a28j-a11.html"]
+#sheet.insert_row(insertRow, 8)
+
+# Get a specific row
+#row = sheet.row_values(3)
+#pprint(row)
+
+
+# Update one cell
+#sheet.update_cell(8, 2, "ADIDAS")
+
+# Get a specific column
+#col = sheet.col_values(3)
+#pprint(col)
+
+# Delete the row
+#sheet.delete_row(8)
