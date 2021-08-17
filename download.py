6fb576e389697f38702eb8a6a7b448cd274ea0d4
James Lunt <50912137+james-lunt@users.noreply.github.com>
2021-08-17 20:46:20 +0100

    Converter connected to api
    End of file detection not working

--- download.py
+++ download.py
@@ -1,14 +1,39 @@
 import fitz
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
 import os
+
+# scope of the application
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    "creds.json", scope)
+
+client = gspread.authorize(credentials)
+
+sheet = client.open("Tester").worksheet("Sheet1")
+
 at = 'test.PDF'
 os.system('python -m fitz gettext -pages 1 test.PDF')
 
-my_file = open("test.txt", "r")
-for line in my_file:
-    print(line)
+#input file
+fin = open("test.txt", "rt")
+fout = open("out.txt", "wt")
 
-#for line in my_file:
-	#read replace the string and write to output file
-#	my_file.write(line.replace(' ', '~'))
+for line in fin:
+	fout.write(line.replace(' ', '~'))
+
+fin.close()
+fout.close()
+
+with open('out.txt') as f:
+   for line in f:
+       insertRow = [line]
+       sheet.insert_row(insertRow, 1)
+       #print (line)
+
+
+
 
