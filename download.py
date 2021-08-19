32d718680b1c572bbac06e31255c583411b54c94
James Lunt <50912137+james-lunt@users.noreply.github.com>
2021-08-19 13:23:16 +0100

    Updates to sheet in a correct format
    eplacing spaces with tildas, stripping new line characters and indicating the end of a page with EOP

--- download.py
+++ download.py
@@ -2,7 +2,7 @@ import fitz
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
-import os
+import os, sys
 import numpy as np
 
 # scope of the application
@@ -17,40 +17,26 @@ client = gspread.authorize(credentials)
 sheet = client.open("Tester").worksheet("Sheet2")
 sheet.clear()
 
-empty_array = np.empty((0, 1),str)
-with open('out.txt') as f:
-   
-   #first = True
-   for line in f:
-       empty_array = np.append(empty_array, np.array([[line]]), axis=0)
 
-#print(empty_array) 
+#convert pdf to text
+at = 'test.PDF'
+os.system('python -m fitz gettext test.PDF')
+
+#create empty 2D array
+empty_array = np.empty((0, 1),str)
+
+#open covnerted text file
+with open('test.txt') as f:
+    #iterate through each line
+    for line in f:
+        #if line has values in it other than just a new character
+        if line != ('\n'):
+            #append line to 2D array while replacing spaces with tildas, stripping new line characters and indicating the end of a page with EOP
+            empty_array = np.append(empty_array, np.array([[line.replace(' ','~').replace('\n','').replace('\x0c','EOP')]]), axis=0)
+
 
 # Write the array to worksheet starting from the 1st cell
 sheet.update('A1', empty_array.tolist())
-
-"""
-at = 'test.PDF'
-os.system('python -m fitz gettext -pages 1 test.PDF')
-
-#input file
-fin = open("test.txt", "rt")
-fout = open("out.txt", "wt")
- 
-for line in fin:
-	fout.write(line.replace(' ', '~'))
-
-fin.close()
-fout.close()
-
-with open('out.txt') as f:
-   for line in f:
-    #insertRow = [line]
-    #sheet.insert_row(insertRow, 1)
-       print (line)
-"""
-
-
-
-
+#close text file
+f.close()
 
