6548886462a92a295936a65b84b9a13470978d23
James Lunt <50912137+james-lunt@users.noreply.github.com>
2021-08-19 18:27:03 +0100

    Irrelevant files deleted and deletion methods implemented

--- main.py
+++ main.py
@@ -23,46 +23,56 @@ client = gspread.authorize(credentials)
 
 
 #---------------------------------Check for new invoices------------------------------------
-#Open inbound invoices sheet
-invoices = client.open("Copy of sample PDF data rows").worksheet("incoming")
+invoices = client.open("sample PDF data rows").worksheet("incoming")
+invoice_values = invoices.get_all_values()
+process_list = []
+for invoice in invoice_values:
+    #Boolean value in column 6
+    if invoice[6] == 'TRUE':
+        process_list.append(invoice)
 
-current_rows = len(invoices.col_values(1))
-#infinite loop to check for new invoices
-while(True):
-    if len(invoices.col_values(1)) > current_rows:
-        index = invoices.col_values(1)
-        break 
-
-
+#print(process_list)
 
 
-#---------------------Download PDF from url and Convert to text-----------------------------
-pdf_name = invoices.cell(index, 6).value
-pdf = requests.get(pdf_name, allow_redirects=True)
+#------------------------------------Download and Convert PDF------------------------------
+for invoice in process_list:
+    #get url and name for pdf
+    pdf_name = invoice[1]
+    pdf_url = invoice[3]
+    #download pdf   
+    r = requests.get(pdf_url, allow_redirects=True)
+    #write pdf to local machine with correct name
+    open(pdf_name, 'wb').write(r.content)
 
-#convert pdf to text
-os.system('python -m fitz gettext %s',pdf)
 
+    #convert pdf to text
+    os.system('python -m fitz gettext {}'.format(pdf_name))
 
+    text_name = pdf_name.replace('.PDF', '.txt')
 
 
 #---------------------Tilda parse data and write it to google sheets-----------------------
-#create empty 2D array
-empty_array = np.empty((0, 1),str)
+    #create empty 2D array
+    empty_array = np.empty((0, 1),str)
 
-#open converted text file
-with open(os.system('python -m fitz gettext %s',pdf)) as f:
-    #iterate through each line
-    for line in f:
-        #if line has values in it other than just a new character
-        if line != ('\n'):
-            #append line to 2D array while replacing spaces with tildas, stripping new line characters and indicating the end of a page with EOP
-            empty_array = np.append(empty_array, np.array([[line.replace(' ','~').replace('\n','').replace('\x0c','EOP')]]), axis=0)
+    #open converted text file
+    with open(text_name) as f:
+        #iterate through each line
+        for line in f:
+            #if line has values in it other than just a new character
+            if line != ('\n'):
+                #append line to 2D array while replacing spaces with tildas, stripping new line characters and indicating the end of a page with EOP
+                empty_array = np.append(empty_array, np.array([[line.replace(' ','~').replace('\n','').replace('\x0c','EOP')]]), axis=0)
 
-#Open Outbound invoices sheet
-outbound = client.open("Output").worksheet("Sheet1")
+    #Open Outbound invoices sheet
+    outbound = client.open("Output").worksheet("Sheet1")
+    outbound.clear() #clear sheet for testing
 
-# Write the array to worksheet starting from the 1st cell
-outbound.update('A1', empty_array.tolist())
-#close text file
-f.close()
+    # Write the array to worksheet starting from the 1st cell
+    outbound.update('A1', empty_array.tolist())
+    #close text file
+    f.close()
+
+#-----------------------------Delete text and pdf off local machine-----------------------
+    os.remove(text_name)
+    os.remove(pdf_name)
